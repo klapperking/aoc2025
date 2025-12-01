f5f5b935aac22925054cd86c973bbb6818cba984
Martin Klapper <64156820+klapperking@users.noreply.github.com>
2025-12-01 09:55:04 +0100

    setup pythong environment

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
+{
+  cachix.enable = false;
+  difftastic.enable = true;
+
+  packages = [ pkgs-unstable.git ];
+
+  languages.python = {
+    enable = true;
+    package = pkgs-unstable.python313;
+    uv = {
+      enable = true;
+    };
+  };
+}
