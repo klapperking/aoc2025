bc7fe9cb6cbd1b1657b01e6a2b1e5b40fcb76b94
Martin Klapper <64156820+klapperking@users.noreply.github.com>
2025-12-04 09:42:10 +0100

    brute force part 2 - nothing smart

--- day04/solution.py
+++ day04/solution.py
@@ -14,36 +14,55 @@ def parse_input(input_data: str) -> list[list[str]]:
 def calculate_solution(rows: list[list[str]], battery_sequence_size: int = 2) -> int:
     movable_papers = 0
 
-    for row_idx in range(len(rows)):
-        for col_idx in range(len(rows[row_idx])):
-            adjacent = 0
+    removed_last_pass = True
+    removed_positions: list[list[int]] = []
 
-            # all directions
-            for x in range(-1, 2):
-                for y in range(-1, 2):
-                    check_row = row_idx + x
-                    check_col = col_idx + y
+    while removed_last_pass:
+        removed_last_pass = False
 
-                    # skip outside grid and current position
-                    if (
-                        (x == 0 and y == 0)
-                        or check_row < 0
-                        or check_row >= len(rows)
-                        or check_col < 0
-                        or check_col >= len(rows[0])
-                    ):
-                        continue
+        for pos in removed_positions:
+            rows[pos[0]][pos[1]] = "."
 
-                    if rows[check_row][check_col] == "@":
-                        adjacent += 1
+        removed_positions = []
 
-            if adjacent < 4 and rows[row_idx][col_idx] == "@":
-                movable_papers += 1
+        for row_idx in range(len(rows)):
+            for col_idx in range(len(rows[row_idx])):
+                adjacent = 0
+
+                # all directions
+                for x in range(-1, 2):
+                    for y in range(-1, 2):
+                        check_row = row_idx + x
+                        check_col = col_idx + y
+
+                        # skip outside grid and current position
+                        if (
+                            (x == 0 and y == 0)
+                            or check_row < 0
+                            or check_row >= len(rows)
+                            or check_col < 0
+                            or check_col >= len(rows[0])
+                        ):
+                            continue
+
+                        if rows[check_row][check_col] == "@":
+                            adjacent += 1
+
+                if adjacent < 4 and rows[row_idx][col_idx] == "@":
+                    removed_positions.append([row_idx, col_idx])
+                    removed_last_pass = True
+                    movable_papers += 1
 
     return movable_papers
 
 
 def part1(input_data: str) -> None:
+    rows = parse_input(input_data)
+    result = calculate_solution(rows)
+    print(result)
+
+
+def part2(input_data: str) -> None:
     rows = parse_input(input_data)
     result = calculate_solution(rows)
     print(result)
@@ -58,9 +77,6 @@ if __name__ == "__main__":
     input_data = sys.stdin.read()
 
     if args.part == 1:
-        part1(input_data)
+        part2(input_data)
     else:
-        pass
-
-
-# should be 13
+        part2(input_data)
